c959af5d82d10c8b9ffb9e88d9dd0985bc2aebaa
Vasiliy <102950888+vasiliy19-12-1997@users.noreply.github.com>
2025-05-12 14:05:25 +0500

    1

--- config/storybook/webpack.config.ts
+++ config/storybook/webpack.config.ts
@@ -3,30 +3,41 @@ import path from 'path';
 import { buildCssLoader } from '../build/loaders/buildCssLoader';
 import { BuildPaths } from '../build/types/config';
 
-export default ({ config }: {config: webpack.Configuration}) => {
+export default ({ config }: { config: webpack.Configuration }) => {
     const paths: BuildPaths = {
         build: '',
         html: '',
         entry: '',
         src: path.resolve(__dirname, '..', '..', 'src'),
     };
-    config.resolve.modules.push(paths.src);
-    config.resolve.extensions.push('.ts', '.tsx');
 
-    // eslint-disable-next-line no-param-reassign
-    config.module.rules = config.module.rules.map((rule: RuleSetRule) => {
-        if (/svg/.test(rule.test as string)) {
+    if (config.resolve) {
+        config.resolve.modules = [
+            ...(config.resolve.modules || []),
+            paths.src,
+            'node_modules',
+        ];
+        config.resolve.extensions = [
+            ...(config.resolve.extensions || []),
+            '.ts',
+            '.tsx',
+        ];
+    }
+
+    config.module.rules = config.module.rules?.map((rule: RuleSetRule) => {
+        if (rule.test instanceof RegExp && rule.test.test('.svg')) {
             return { ...rule, exclude: /\.svg$/i };
         }
-
         return rule;
-    });
+    }) || [];
 
-    config.module.rules.push({
-        test: /\.svg$/,
-        use: ['@svgr/webpack'],
-    });
-    config.module.rules.push(buildCssLoader(true));
+    config.module.rules.push(
+        {
+            test: /\.svg$/,
+            use: ['@svgr/webpack'],
+        },
+        buildCssLoader(true),
+    );
 
     return config;
 };
