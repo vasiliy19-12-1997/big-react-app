3960742a1dcf51ad8f59629eaee6925ef0175116
Vasiliy <102950888+vasiliy19-12-1997@users.noreply.github.com>
2025-05-20 07:18:26 +0500

    1

---
+++ src/shared/ui/Modal/Modal.tsx
@@ -0,0 +1,67 @@
+import {
+    ReactNode, useCallback, useEffect, useRef, useState,
+} from 'react';
+import { classNames } from 'shared/lib/classNames/classNames';
+import cls from './Modal.module.scss';
+
+interface ModalProps {
+  className?: string;
+  children?:ReactNode;
+  isOpen?:boolean;
+    onClose?:()=>void;
+}
+const ANIMATION_DELAY = 300;
+export const Modal = (props: ModalProps) => {
+    const {
+        className,
+        children,
+        isOpen,
+        onClose,
+    } = props;
+    const [isClosing, setIsClosing] = useState(false);
+    const timerRef = useRef<ReturnType<typeof setTimeout>>();
+    const closeHandler = () => {
+        if (onClose) {
+            setIsClosing(true);
+            timerRef.current = setTimeout(() => {
+                onClose();
+                setIsClosing(false);
+            }, ANIMATION_DELAY);
+        }
+    };
+    const mods:Record<string, boolean> = {
+        [cls.opened]: isOpen,
+        [cls.isClosing]: isClosing,
+    };
+    const onContentClick = (e:React.MouseEvent) => {
+        e.stopPropagation();
+    };
+    const onKeyDown = useCallback((e:KeyboardEvent) => {
+        if (e.key === 'Escape') {
+            closeHandler();
+        }
+    }, [closeHandler]);
+    useEffect(() => {
+        if (isOpen) {
+            window.addEventListener('keydown', onKeyDown);
+        }
+        return () => {
+            window.removeEventListener('keydown', onKeyDown);
+            clearInterval(timerRef.current);
+        };
+    }, [isOpen]);
+    return (
+        <div className={classNames(cls.Modal, mods, [className])}>
+            <div className={cls.overlay} onClick={closeHandler}>
+                <div
+                    className={
+                        cls.content
+                    }
+                    onClick={onContentClick}
+                >
+                    {children}
+                </div>
+            </div>
+        </div>
+    );
+};
