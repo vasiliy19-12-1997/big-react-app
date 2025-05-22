2c8924675b32b64fb6f3611ee4e415fab77e0ec3
Vasiliy <102950888+vasiliy19-12-1997@users.noreply.github.com>
2025-05-22 07:49:55 +0500

    fix problems with build

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import { ThemeProvider } from 'app/providers/ThemeProvider';
+import { Theme, ThemeProvider } from 'app/providers/ThemeProvider';
 import App from './app/App';
 import 'app/styles/index.scss';
 import './shared/config/i18n/i18n';
@@ -9,7 +9,7 @@ import { ErrorBoundary } from './app/providers/ErrorBoundary';
 render(
     <BrowserRouter>
         <ErrorBoundary>
-            <ThemeProvider>
+            <ThemeProvider initialTheme={Theme.LIGHT}>
                 <App />
             </ThemeProvider>
         </ErrorBoundary>
