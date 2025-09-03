f96d5bdf1c0508ba5cef7a1c40ce54e724d669e6
Vasiliy <102950888+vasiliy19-12-1997@users.noreply.github.com>
2025-09-03 11:49:32 +0500

    add items virtualized to list

--- src/entities/Article/ui/ArticleList/ArticleList.tsx
+++ src/entities/Article/ui/ArticleList/ArticleList.tsx
@@ -24,25 +24,41 @@ export const ArticleList = memo((props: ArticleListProps) => {
     const {
         className, articles, views = ArticleViews.SMALL, isLoading, target,
     } = props;
-    const getSceletons = () => new Array(views === ArticleViews.SMALL ? 9 : 3).fill(0).map((item, index) => (
-        <ArticleListItemSceleton view={views} key={String(index)} />
+    const getSceletons = (view:ArticleViews) => new Array(view === ArticleViews.SMALL ? 9 : 3).fill(0).map((item, index) => (
+        <ArticleListItemSceleton view={view} key={String(index)} />
     ));
-
+    const isBig = views === ArticleViews.BIG;
+    const itemsPerRow = isBig ? 1 : 3;
+    const rowCount = isBig ? articles.length : Math.ceil(articles.length / itemsPerRow);
     const rowRenderer = ({
-        index, isScrolling, key, style,
+        index,
+        isScrolling,
+        key,
+        style,
     }:ListRowProps) => {
+        const items = [];
+        const fromIndex = index * itemsPerRow;
+        const toIndex = Math.min(fromIndex + itemsPerRow, articles.length);
+        for (let i = fromIndex; i < toIndex; i += 1) {
+            items.push(
+                <ArticleListItem
+                    article={articles[i]}
+                    className={cls.card}
+                    view={views}
+                    target={target}
+                    key={`str${i}`}
+                />,
+            );
+        }
         console.log('render');
+
         return (
             <div
+                className={cls.row}
                 key={key}
                 style={style}
             >
-                <ArticleListItem
-                    className={cls.card}
-                    article={articles[index]}
-                    view={views}
-                    target={target}
-                />
+                {items}
             </div>
         );
     };
@@ -58,19 +74,29 @@ export const ArticleList = memo((props: ArticleListProps) => {
         <WindowScroller
             scrollElement={document.getElementById(PAGE_ID) as Element}
         >
-            {({ height, width }) => (
+            {({
+                height,
+                width,
+                registerChild,
+                isScrolling,
+                onChildScroll,
+                scrollTop,
+            }) => (
 
-                <div className={classNames(cls.ArticleList, {}, [className, cls[views]])}>
+                <div ref={registerChild} className={classNames(cls.ArticleList, {}, [className, cls[views]])}>
                     <List
-                        height={height}
-                        width={width}
-                        rowHeight={500}
-                        rowCount={articles.length}
-                        // eslint-disable-next-line react/no-unstable-nested-components, i18next/no-literal-string
+                        height={height ?? 700}
+                        rowCount={rowCount}
+                        rowHeight={isBig ? 700 : 300}
+                        width={width ? width - 80 : 700}
                         rowRenderer={rowRenderer}
+                        autoHeight
+                        onScroll={onChildScroll}
+                        isScrolling={isScrolling}
+                        scrollTop={scrollTop}
+                        // eslint-disable-next-line react/no-unstable-nested-components, i18next/no-literal-string
                     />
-                    {isLoading && getSceletons()}
-
+                    {isLoading && getSceletons(views)}
                 </div>
             )}
         </WindowScroller>
