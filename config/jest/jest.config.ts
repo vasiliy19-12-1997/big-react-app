f0c79ffdbed6476433eb08619b18a566ff69b839
Vasiliy <102950888+vasiliy19-12-1997@users.noreply.github.com>
2025-09-20 12:26:23 +0500

    add jest report html

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -36,6 +36,15 @@ export default {
         '\\.svg': path.resolve(__dirname, 'jestEmptyComponent.tsx'),
     },
     globals: { __IS_DEV__: true, __API__: '', __PROJECT__: 'jest' },
+    reporters: [
+        'default',
+        ['jest-html-reporters', {
+            publicPath: '<rootDir>/reports/unit',
+            filename: 'report.html',
+            openReport: true,
+            inlineSource: true,
+        }],
+    ],
     // Indicates whether the coverage information should be collected while executing the test
     // collectCoverage: false,
 
