debaf9ac96f5a717acda70e98e2c28d51a5306a9
Коновалов Василий Сергеевич <Wasyok.ru@yandex.ru>
2025-10-24 16:55:44 +0500

    fix funny error

--- src/shared/ui/Stack/HStack/HStack.tsx
+++ src/shared/ui/Stack/HStack/HStack.tsx
@@ -1,12 +1,9 @@
 import { Flex, FlexProps } from '../Flex/Flex';
-import { ErrorPage } from '@/widgets/ErrorPage';
 
 type HStackProps = Omit<FlexProps, 'direction'>;
 
 export const HStack = (props: HStackProps) => {
     return (
-        <Flex direction="row" {...props}>
-            <ErrorPage />
-        </Flex>
+        <Flex direction="row" {...props} />
     );
 };
