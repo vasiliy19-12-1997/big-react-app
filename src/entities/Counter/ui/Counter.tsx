1e41d8350e326374175152ffd64e8a96cc881fbe
Коновалов Василий Сергеевич <Wasyok.ru@yandex.ru>
2025-12-09 14:43:20 +0500

    1

--- src/entities/Counter/ui/Counter.tsx
+++ src/entities/Counter/ui/Counter.tsx
@@ -1,11 +1,9 @@
-import { useDispatch } from 'react-redux';
 import { useTranslation } from 'react-i18next';
 import { Button } from '@/shared/ui/Button';
 import { useCounterValue } from '../model/selectors/getCounterValue/getCounterValue';
 import { useCounterActions } from '../model/slice/counterSlice';
 
 export const Counter = () => {
-    const dispatch = useDispatch();
     const counterValue = useCounterValue();
     const { t } = useTranslation();
     const { add, decrement, increment } = useCounterActions();
