3c7afd88b8a0c33c71f5481d58fe47739ee0f17e
Vasiliy <102950888+vasiliy19-12-1997@users.noreply.github.com>
2025-12-28 13:27:53 +0500

    1

--- cypress/e2e/articles/article-details.cy.ts
+++ cypress/e2e/articles/article-details.cy.ts
@@ -1,29 +1,34 @@
 let currentArticleId = '';
-describe('Пользователь заходит на страницу статьи', () => {
+describe('Тесты статей', () => {
+  describe('Работающие тесты', ()=>{
   beforeEach(() => {
-      cy.login();
-      cy.createArticle().then((article)=>{
-        cy.log(JSON.stringify(article))
-        currentArticleId =  article.id
-        cy.visit(`articles/${article.id}`)
+        cy.login();
+        cy.createArticle().then((article)=>{
+          currentArticleId =  article.id
+          cy.visit(`articles/${article.id}`)
+        })
+    })
+    // Создали статью протестили что нужно и удалили полотом
+      afterEach(()=>{
+          cy.removeArticle(currentArticleId)
       })
-   })
-  // Создали статью протестили что нужно и удалили полотом
-    afterEach(()=>{
-      if(currentArticleId){
-        cy.removeArticle(currentArticleId)
-      }
+    it('Видно содержимое статьи', () => {
+      cy.getByTestId('ArticleDetails.Info').should('exist')
     })
-  it('Видно содержимое статьи', () => {
-    cy.getByTestId('ArticleDetails.Info').should('exist')
-  })
-  it('Видно список рекомендаций', () => {
-    cy.getByTestId('ArticleRecommendationList').should('exist')
-  })
-  it('Видно блок с комментариями и оставляется комментарий', () => {
-    cy.getByTestId('CommentaryCard').should('exist')
-    cy.getByTestId('AddCommentForm').scrollIntoView()
-    cy.addComment("test")
-    cy.getByTestId('CommentaryCard.Text.Paragraph').should('have.length.greaterThan', 20)
+    it('Видно список рекомендаций', () => {
+      cy.getByTestId('ArticleRecommendationList').should('exist')
     })
+    it('Видно блок с комментариями и оставляется комментарий', () => {
+      cy.getByTestId('CommentaryCard').should('exist')
+      cy.getByTestId('AddCommentForm').scrollIntoView()
+      cy.addComment("test")
+      cy.getByTestId('CommentaryCard.Text.Paragraph').should('have.length.greaterThan', 20)
+      })
+  })
+  describe('Нерабочие тесты', ()=>{
+//скипаем этот тест, чтобы залить релиз
+  it.skip('Видно блок с комментариями и оставляется комментарий', () => {
+    cy.getByTestId('CommentaryCardfdfd').should('exist')
+ })
+})
 })
