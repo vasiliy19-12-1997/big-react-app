81a4ae0c49a0a6f80edf85d55819c2ed8d691929
Vasiliy <102950888+vasiliy19-12-1997@users.noreply.github.com>
2026-01-10 14:04:34 +0500

    add proxy query params to nginx

--- json-server/index.js
+++ json-server/index.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const jsonServer = require('json-server');
 const path = require('path');
 const https = require('https');
+const http = require('http');
 
 const options = {
     key: fs.readFileSync(path.resolve(__dirname, 'server.key')),
@@ -57,7 +58,13 @@ server.use(router);
 
 // запуск сервера
 const PORT = 8443;
+const HTTP_PORT = 8000;
 const httpsServer = https.createServer(options, server);
+const httpServer = http.createServer(server);
+
 httpsServer.listen(PORT, () => {
+    console.log(`server is running on ${PORT} port`);
+});
+httpServer.listen(HTTP_PORT, () => {
     console.log(`server is running on ${PORT} port`);
 });
